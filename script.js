69be9b0216787488d64fdc7f32fa3b79358286fa
insomniaZzz0 <160507585+insomniaZzz0@users.noreply.github.com>
2024-06-20 09:01:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function somar(){
+    let num1=8;
+    let num2=12;
+    let result=num1+num2;
+    document.getElementById("receptor").innerHTML=result
+}
+ function sub(){
+    let num1=8;
+    let num2=12;
+    let result=num1-num2;
+    document.getElementById("receptor").innerHTML=result
+ }
+
+ function mult(){
+    let num1=8;
+    let num2=12;
+    let result=num1*num2;
+    document.getElementById("receptor").innerHTML=result
+
+ }
+
+ function div(){
+    let num1=8;
+    let num2=12;
+    let result=num1/num2;
+    document.getElementById("receptor").innerHTML=result
+
+ }
